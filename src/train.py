977e24148e6dd7e9b47e2c3bf785cd2340371f1e
breadone <juniper@breadone.xyz>
2024-10-02 14:26:46 +1300

    initial train/test set splitting

--- src/train.py
+++ src/train.py
@@ -8,9 +8,36 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 import pathlib
-data_dir = pathlib.Path('../train').with_suffix('')
 
-# image_count = len(list(data_dir.glob('*/*.png')))
+def train():
+    batch_size = 32
+    img_height = 720
+    img_width = 1280
 
-aw = list(data_dir.glob('Animal Weaknesses/*'))
-PIL.Image.open(str(aw[0]))
+    data_dir = pathlib.Path('../train').with_suffix('')
+
+    # split into training and validation set with a 20% split
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="training",
+            seed=0,
+            image_size=(img_height, img_width),
+            batch_size=batch_size
+        )
+
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size
+        )
+    
+    
+
+
+
+if __name__ == '__main__':
+    train()
